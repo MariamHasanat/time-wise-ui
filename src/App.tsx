07cfca92f141f559b96bb585ad5d16665a07c46a
Manolya Tamimi <tamimimanolya@gmail.com>
2023-04-14 23:00:02 +0300

    initializing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h3 style={{ "fontSize": "var(--header-font-size)" }}>Header</h3>
-      <h3 style={{ "fontSize": "var(--subheader-font-size)" }}>Subheader</h3>
-      <p style={{ "fontSize": "var(--body-font-size)" }}>body</p>
-      <p style={{ "fontSize": "var(--body-font-size)", "color":"var(--grey-font-color)" }}>grey body</p>
-
+      <BrowserRouter>
+      {/* <Header/> */}
+        <Routes>
+          {/* <Route path='login' element={<Login/>}/> */}
+          {/* <Route path='dashboard' element={<Dashboard/>}/> */}
+          {/* <Route path='timeTracker' element={<TimeTracker/>}/> */}
+          {/* <Route path='projects' element={<Projects/>}/> */}
+        </Routes>
+        {/* <Footer/> */}
+      </BrowserRouter>
     </div>
   );
 }
