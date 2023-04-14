10be85ec4b651709a61a09cbcb11b49bd6ea364a
Manolya Tamimi <tamimimanolya@gmail.com>
2023-04-14 23:07:46 +0300

    fixing routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,11 @@ function App() {
       <BrowserRouter>
       {/* <Header/> */}
         <Routes>
-          {/* <Route path='login' element={<Login/>}/> */}
-          {/* <Route path='dashboard' element={<Dashboard/>}/> */}
-          {/* <Route path='timeTracker' element={<TimeTracker/>}/> */}
-          {/* <Route path='projects' element={<Projects/>}/> */}
+          {/* <Route path='/login' element={<Login/>}/> */}
+          {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
+          {/* <Route path='/timeTracker' element={<TimeTracker/>}/> */}
+          {/* <Route path='/projects' element={<Projects/>}/> */}
+          <Route path='/'element={<>project is working</>}></Route>
         </Routes>
         {/* <Footer/> */}
       </BrowserRouter>
