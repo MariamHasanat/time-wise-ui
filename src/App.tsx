094affdec3cd0bd83f9369593eb4635737b7239c
sajedaswad <you@example.com>
2023-05-04 12:47:49 +0300

    finish input with css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Input from './components/input/input';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         {/* <Header/> */}
-        <Input/>
+
         <Routes>
           {/* <Route path='/login' element={<Login/>}/> */}
           {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -3,16 +3,17 @@ import React from "react";
 import './input.css'
 
 
-interface InpProps extends React.InputHTMLAttributes< HTMLInputElement> {
+interface InpProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: String;
 }
 
 const Input: React.FC<InpProps> = (props) => {
   return (
     <div className="input">
-    <input {...props}>{props.label}</input>
+      <input {...props}>{props.label}</input>
     </div>
   )
 }
 
 export default Input;
+
