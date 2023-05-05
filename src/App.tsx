536712453706d40925eb584b2fb777c52fbbec52
sajeda swad <201047@ppu.edu.ps>
2023-05-05 19:47:56 +0200

    start doing project interface

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
         {/* <Header/> */}
-        <ProjectComponent/>
         <Routes>
           {/* <Route path='/login' element={<Login/>}/> */}
           {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
