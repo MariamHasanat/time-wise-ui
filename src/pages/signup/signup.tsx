4b11bac7d00bf24328580a0b7233e58c60605840
Manolya Tamimi <tamimimanolya@gmail.com>
2023-05-07 23:59:47 +0300

    almost done, just need to style the input and edit the logo color

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -5,12 +5,19 @@ import Input from '../../components/input/input';
 const Signup = () => {
   return (
     <div className="signup">
-      <h3>sign up</h3>
-      <Input label="username" placeholder='hello'/>
-      <Input label="email" type='email'/>
-      <Input label="password" type='password'/>
-      <Input label="re-enter password" type='password'/>
-      <Button label="sign up" primary/>
+      <div className='signup-logo'>
+        <img src="logo.png" alt="logo" width={70} />
+        <h4>time wise</h4>
+      </div>
+      <h3>Create a new  Account</h3>
+      <form action="">
+        <Input label="Username" />
+        <Input label="Email" type='email' />
+        <Input label="Password" type='password' />
+        <Input label="Confirm Password" type='password' />
+        <Input type='checkbox' label="show password" style={{ "color": "var(--primary-color)" }} />
+        <Button label="Sign up" primary />
+      </form>
     </div>
   )
 }
