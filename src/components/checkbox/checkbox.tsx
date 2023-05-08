1b4ef89c58b1695573e752cd3fff7d0101e605f1
Manolya Tamimi <tamimimanolya@gmail.com>
2023-05-08 20:06:48 +0300

    adding checked state on checkbox

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -1,14 +1,14 @@
-import React from "react";
 import './checkbox.css'
-
+import React from "react";
 interface checkBProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: String;
 }
 
 const Checkbox: React.FC<checkBProps> = (props) => {
+  const [checked, setChecked] = React.useState<boolean>(false);
   return (
-    <div className="checkbox">
-      <input {...props} type="checkbox" />
+    <div className="checkbox" onClick={() => setChecked(!checked)}>
+      <input {...props} type="checkbox" checked={checked} />
       {props.label && <label>{props.label}</label>}
     </div>
   )
