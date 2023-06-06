9b3add3514921c54d3ccfc0b833b53a0fcb5227e
Mariam <201011@ppu.edu.ps>
2023-06-06 12:51:04 +0300

    modify the new-task-form component and import the files needed

--- src/components/new-task-form/new-task-form.tsx
+++ src/components/new-task-form/new-task-form.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Input } from 'antd';
+import DropDown from '../drop-down/drop-down';
+import StopWatch from '../stop-watch/stop-watch';
+import { PlayCircleOutlined } from '@ant-design/icons';
+
 
 const NewTaskForm: React.FC = () => {
   return (
     <form>
-      <div>
-        <Input placeholder="Task Description" />;
-      </div>
+      <Input placeholder="Task Description" />
+      <DropDown />
+      <StopWatch />
+      <button>
+        <PlayCircleOutlined />
+      </button>
     </form>
   )
 }
