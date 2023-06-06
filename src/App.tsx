fe97be20786156b248cdf5eb063acf31a7d143e5
Manolya Tamimi <tamimimanolya@gmail.com>
2023-06-06 12:27:18 +0300

    adding layout and surrounding pages with it

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Signup from './pages/signup/signup';
 import ProjectsPage from './pages/projects/projects';
 import TimeTracker from './pages/time-tracker/time-tracker';
+import PageLayout from './components/layout/layout';
 
 function App() {
   return (
@@ -11,11 +12,11 @@ function App() {
         {/* <Header/> */}
         <Routes>
           <Route path='/signup' element={<Signup />} />
-          {/* <Route path='/login' element={<Login/>}/> */}
-          {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
-          <Route path='/timeTracker' element={<TimeTracker />} />
-          <Route path='/projects' element={<ProjectsPage />} />
-          <Route path='/' element={<>project is working</>}></Route>
+          {/* <Route path='/login' element={<PageLayout><Login/></PageLayout>}/> */}
+          {/* <Route path='/dashboard' element={<PageLayout><Dashboard/></PageLayout>}/> */}
+          <Route path='/timeTracker' element={<PageLayout><TimeTracker /></PageLayout>} />
+          <Route path='/projects' element={<PageLayout><ProjectsPage /></PageLayout>} />
+          {/* <Route path='/' element={<>project is working</>}></Route> */}
         </Routes>
         {/* <Footer/> */}
       </BrowserRouter>
