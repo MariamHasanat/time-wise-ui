2833e82a16f41926b494ef6e4d9f4a13971f8753
Mariam <201011@ppu.edu.ps>
2023-06-08 12:24:41 +0300

    import the file of calculate time in seconds

--- src/components/stop-watch/stop-watch.tsx
+++ src/components/stop-watch/stop-watch.tsx
@@ -1,20 +1,21 @@
 import React, { useEffect } from 'react'
 import './stop-watch.css';
+import calculateTimeInSeconds from '../../services/calculate-time';
 const StopWatch: React.FC = () => {
   const [timeInSecond] = React.useState<number>(0); // i deleted setTimeInSecond :)
-  // const [timeArray, setTimeArray] = React.useState<Array<number | string>>([]);
+  const [timeArray, setTimeArray] = React.useState<Array<number | string>>([]);
 
   useEffect(() => {
-    //  setTimeArray(calculateTimeInSeconds(timeInSecond))
+    setTimeArray(calculateTimeInSeconds(timeInSecond));
   }, [timeInSecond]);
   return (
     <label className='stopwatch-container'>
       <div className='timer-display'>
-        <p id='hour'>00</p>
+        <p id='hour'>{timeArray[0]}</p>
         <span>:</span>
-        <p id='minute'>00</p>
+        <p id='minute'>{timeArray[1]}</p>
         <span>:</span>
-        <p id='second'>00</p>
+        <p id='second'>{timeArray[2]}</p>
       </div>
     </label>
   )
