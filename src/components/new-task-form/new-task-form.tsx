4cb94f13963620f2d1ce77bb60766d71b30d77d7
Mariam <201011@ppu.edu.ps>
2023-06-08 12:23:12 +0300

    change the icon of the button

--- src/components/new-task-form/new-task-form.tsx
+++ src/components/new-task-form/new-task-form.tsx
@@ -3,16 +3,34 @@ import './new-task-form.css';
 import { Input, Button } from 'antd';
 import DropDown from '../drop-down/drop-down';
 import StopWatch from '../stop-watch/stop-watch';
-import { PlayCircleOutlined } from '@ant-design/icons';
+import { PlayCircleOutlined, PauseCircleOutlined } from '@ant-design/icons';
 
 
 const NewTaskForm: React.FC = () => {
+  const [buttonFlag, setButtonFlag] = React.useState<boolean>(true);
   return (
     <form className='new-task-form'>
-      <Input placeholder="Task Description" type='string' style={{ width: '300px', marginRight: '50px', height: '30px'}} />
+      <Input placeholder="Task Description" type='string' style={{ width: '300px', marginRight: '50px', height: '30px' }} />
       <DropDown />
       <StopWatch />
-      <Button type='primary' style={{ height: '30px', width: '30px', borderRadius: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}><PlayCircleOutlined style={{ fontSize: '18px' }} /></Button>
+      {
+        buttonFlag
+          ?
+          <Button
+            onClick={(e) => {
+              e.preventDefault()
+              setButtonFlag(!buttonFlag);
+            }}
+            type='primary' style={{ height: '30px', width: '30px', borderRadius: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}><PlayCircleOutlined style={{ fontSize: '18px' }} /></Button>
+          :
+          <Button
+            onClick={(e) => {
+              e.preventDefault()
+              setButtonFlag(!buttonFlag);
+            }}
+            type='primary' style={{ height: '30px', width: '30px', borderRadius: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}><PauseCircleOutlined style={{ fontSize: '18px' }} /></Button>
+      }
+
     </form>
   )
 }
