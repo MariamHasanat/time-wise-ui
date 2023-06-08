c19ddc5e5dc51fa720a51043d0f0632a47ed2bde
Manolya Tamimi <tamimimanolya@gmail.com>
2023-06-08 13:39:32 +0300

    changing font colors

--- src/components/task-log/task-log.tsx
+++ src/components/task-log/task-log.tsx
@@ -7,8 +7,8 @@ const TaskLog = () => {
     <form className='task-log'>
         <span className="color">&nbsp;</span>
       <div className='task-desc'>
-        <label style={{ fontSize: '15px', color: '#7489C1', width: 300 }}>Description</label>
-        <label style={{ fontSize: '10px', color: '#52469C' }} >projectName</label>
+        <label style={{ fontSize: '15px', color: '#52469C', width: 300 }}>Description</label>
+        <label style={{ fontSize: '10px', color: '#7489C1' }} >projectName</label>
       </div>
       <label >0 mins</label>
       <label> <ClockCircleOutlined /> &nbsp;3:30 - 3:45pm</label>
