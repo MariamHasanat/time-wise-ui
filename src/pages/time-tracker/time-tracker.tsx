a9e67f450b7fe1a5333264180d00e1774a6d3b86
Manolya Tamimi <tamimimanolya@gmail.com>
2023-06-12 00:20:46 +0300

    adding showmessage function

--- src/pages/time-tracker/time-tracker.tsx
+++ src/pages/time-tracker/time-tracker.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import './time-tracker.css';
 import NewTaskForm from '../../components/new-task-form/new-task-form';
 import TaskLog from '../../components/task-log/task-log';
-import Message from '../../components/message/message';
 
 const TimeTracker: React.FC = () => {
   return (
     <div className='time-tracker'>
-      <Message message="hello" type="loading"/>
       <NewTaskForm />
       <TaskLog />
     </div>
