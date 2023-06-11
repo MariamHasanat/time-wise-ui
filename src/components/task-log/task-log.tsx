b8992595444b38f84881523896d3f38e808240a3
Manolya Tamimi <tamimimanolya@gmail.com>
2023-06-11 12:45:13 +0300

    adding dropdown and modals for editing and deleting tasks

--- src/components/task-log/task-log.tsx
+++ src/components/task-log/task-log.tsx
@@ -1,18 +1,44 @@
-import './task-log.css';
-import { DatePicker } from 'antd';
-import { ClockCircleOutlined } from '@ant-design/icons'
+import "./task-log.css";
+import { Button, DatePicker, Modal } from "antd";
+import { ClockCircleOutlined, MoreOutlined } from "@ant-design/icons"
+import { useState } from "react";
+import { Dropdown } from "antd";
+import type { MenuProps } from 'antd';
 
 const TaskLog = () => {
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const items: MenuProps['items'] = [
+    {
+      key: '1',
+      label: (
+        <button onClick={() => setEditMode(true)} style={{ "backgroundColor": "transparent", "border": "none" }}>
+          Edit
+        </button>
+      ),
+    },
+    {
+      key: '2',
+      label: (
+        <button style={{ "backgroundColor": "transparent", "border": "none" }}>
+          Delete
+        </button>
+      ),
+    }
+  ];
   return (
-    <form className='task-log'>
-        <span className="color">&nbsp;</span>
-      <div className='task-desc'>
-        <label style={{ fontSize: '15px', color: '#52469C', width: 300 }}>Description</label>
-        <label style={{ fontSize: '10px', color: '#7489C1' }} >projectName</label>
+    <form className="task-log">
+      <span className="color">&nbsp;</span>
+      <div className="task-desc">
+        <label style={{ fontSize: "15px", color: "#52469C", width: 300 }}>Description</label>
+        <label style={{ fontSize: "10px", color: "#7489C1" }} >projectName</label>
       </div>
       <label >15 mins</label>
       <label> <ClockCircleOutlined /> &nbsp;3:30 - 3:45pm</label>
       <DatePicker />
+      <Modal title="edit task" open={editMode} onCancel={() => setEditMode(false)}>hello</Modal>
+      <Dropdown trigger={['click']} menu={{ items }} placement="bottomLeft" arrow>
+        <Button className="controlbtn"><MoreOutlined style={{ "fontSize": 15 }} /></Button>
+      </Dropdown>
     </form>
   )
 }
