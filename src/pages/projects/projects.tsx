d38886aa280a868a099a04ef0b01a944adcf0836
Manolya Tamimi <tamimimanolya@gmail.com>
2023-06-14 00:37:40 +0300

    fixnig layout of projects page

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -6,11 +6,13 @@ import ProjectForm from '../../components/project-form/project-form';
 const ProjectsPage = () => {
   const [showPopup, setShowPopup] = React.useState<boolean>(false);
   return (
-    <div className="projects">
-      <div className='new-project-btn'>
-        <Button onClick={() => setShowPopup(!showPopup)}>New Project</Button>
+    <div className="projects-page">
+      <div className="projects-page-inner">
+        <div className='new-project-btn'>
+          <Button onClick={() => setShowPopup(!showPopup)}>New Project</Button>
+        </div>
       </div>
-      
+      {/*list of projects here */}
       <ProjectForm showPopup={showPopup} setShowPopup={setShowPopup} />
     </div >
   )
