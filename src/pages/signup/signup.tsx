a0cb7984a2c7ac8f6b17ba41a0297628a4be4a13
Manolya Tamimi <tamimimanolya@gmail.com>
2023-06-14 22:03:57 +0300

    integrating sign up page to BE

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -3,18 +3,37 @@ import React from 'react';
 import Button from "../../components/button/button";
 import Input from '../../components/input/input';
 import Checkbox from '../../components/checkbox/checkbox';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import showMessage from '../../utils/message/message';
+import { signup } from '../../services/users';
+interface ISignup {
+  username: string,
+  email: string,
+  password: string
+}
+
+const checkPasswords = (data: any) => {
+  return data.get('password1') === data.get('password2');
+}
 
 const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
   const data = new FormData(e.currentTarget);
-  const user = {
-    username: data.get('username'),
-    email: data.get('email'),
-    password: data.get('password')
+  if (!checkPasswords(data)) {
+    showMessage('error', 'passwords do not match');
+  }
+  else {
+    const user: ISignup = {
+      username: data.get('username') as string,
+      email: data.get('email') as string,
+      password: data.get('password1') as string
+    }
+    signup(user);
+    // if everything is ok, go to '/login'
   }
 }
 const Signup = () => {
+  const navigate = useNavigate();
   const [showPassword, setShowPassword] = React.useState<boolean>(false);
   return (
     <div className="signup">
@@ -26,11 +45,11 @@ const Signup = () => {
         </div>
       </div>
       <h3>Create a new  Account</h3>
-      <form onSubmit={(e) => { submitHandler(e) }}>
+      <form onSubmit={(e) => { submitHandler(e); }}>
         <Input name='username' label="Username" required />
         <Input name='email' label="Email" type='email' required />
         <Input name='password1' label="Password" type={showPassword ? "text" : "password"} required />
-        <Input name='passowrd2' label="Confirm Password" type={showPassword ? "text" : "password"} required />
+        <Input name='password2' label="Confirm Password" type={showPassword ? "text" : "password"} required />
         <Checkbox label="show password" style={{ "color": "var(--primary-color)" }} checked={showPassword} setChecked={setShowPassword} />
         <Button label="Sign up" primary={true} />
         <Link to="/login">already have an account?</Link>
---
+++ src/services/users.ts
@@ -0,0 +1,31 @@
+import showMessage from "../utils/message/message";
+
+interface ISignup {
+  username: string,
+  email: string,
+  password: string
+}
+
+const signup = async (props: ISignup) => {
+  return fetch('http://localhost:3001/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ...props })
+  })
+    .then(async response => {
+      if (response.status === 201) {
+        showMessage('success', 'success')
+      } else if (response.status === 400) {
+        showMessage('error', 'email already exists\nplease try another one');
+      } else {
+        showMessage('error', 'an unexpected error occured')
+        // throw new Error('Unexpected response status');
+      }
+    })
+    .catch(error => {
+      showMessage('error', error);
+    });
+};
+export { signup };
