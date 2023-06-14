5e9a419578dafde122cb80b7c1f0b95c410bfbcd
Manolya Tamimi <tamimimanolya@gmail.com>
2023-06-15 00:20:48 +0300

    integrating login with BE

---
+++ src/services/user/login.ts
@@ -0,0 +1,33 @@
+import showMessage from "../../utils/message/message";
+
+interface ILogin {
+  email: string,
+  password: string
+}
+
+const login = async (props: ILogin) => {
+  return fetch('http://localhost:3001/signin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ...props })
+  })
+    .then(async response => {
+      if (response.status === 200) {
+        showMessage('success', 'success')
+        return true;
+      } else if (response.status === 400 || response.status === 401) {
+        showMessage('error', 'email/password combination is not valid');
+        return false;
+      } else {
+        showMessage('error', 'an unexpected error occured')
+        throw new Error('Unexpected response status');
+      }
+    })
+    .catch(error => {
+      showMessage('error', error);
+      return false
+    });
+};
+export { login };
