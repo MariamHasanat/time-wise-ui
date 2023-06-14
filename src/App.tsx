c8226815ea555360a9f11172a72789712e247493
Manolya Tamimi <tamimimanolya@gmail.com>
2023-06-14 20:38:41 +0300

    adding some routers and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { ConfigProvider } from 'antd';
+import { Navigate } from "react-router-dom";
 import Signup from './pages/signup/signup';
 import ProjectsPage from './pages/projects/projects';
 import TimeTracker from './pages/time-tracker/time-tracker';
@@ -19,7 +20,7 @@ function App() {
                 colorBgHeader: "#c4d3fa",
                 colorBgBase: '#c4d3fa',
                 colorBgBody: '#f1f8ff',
-                
+
               }, Input: {
                 borderRadius: 4,
                 colorBgBase: '#f1f8ff'
@@ -34,11 +35,11 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/signup' element={<Signup />} />
-            <Route path='/login' element={<Login/>}/>
+            <Route path='/login' element={<Login />} />
             <Route path='/dashboard' element={<PageLayout><Dashboard /></PageLayout>} />
             <Route path='/timeTracker' element={<PageLayout><TimeTracker /></PageLayout>} />
             <Route path='/projects' element={<PageLayout><ProjectsPage /></PageLayout>} />
-            <Route path='/' element={<>project is working</>}></Route>
+            <Route path='/*' element={<Navigate to="/login"/>} />
           </Routes>
         </BrowserRouter>
       </ConfigProvider>
--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Button from "../../components/button/button";
 import Input from '../../components/input/input';
 import Checkbox from '../../components/checkbox/checkbox';
+import { Link } from 'react-router-dom';
 
 const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
@@ -25,13 +26,14 @@ const Signup = () => {
         </div>
       </div>
       <h3>Create a new  Account</h3>
-      <form onSubmit={(e) => {submitHandler(e)}}>
+      <form onSubmit={(e) => { submitHandler(e) }}>
         <Input name='username' label="Username" required />
         <Input name='email' label="Email" type='email' required />
         <Input name='password1' label="Password" type={showPassword ? "text" : "password"} required />
         <Input name='passowrd2' label="Confirm Password" type={showPassword ? "text" : "password"} required />
         <Checkbox label="show password" style={{ "color": "var(--primary-color)" }} checked={showPassword} setChecked={setShowPassword} />
         <Button label="Sign up" primary />
+        <Link to="/login">already have an account?</Link>
       </form>
     </div>
   )
