4dba301cd8aebf88f38a909ff21749896995f54c
Manolya Tamimi <tamimimanolya@gmail.com>
2023-06-15 23:04:01 +0300

    getting data from fields

--- src/components/project-form/project-form.tsx
+++ src/components/project-form/project-form.tsx
@@ -1,26 +1,42 @@
 import './project-form.css'
 import Input from '../input/input';
-import { Modal } from 'antd';
+import { Modal, Form, Button } from 'antd';
 
 interface iProps {
   showPopup: boolean;
   setShowPopup: (arg0: boolean) => void;
 };
+const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  const data = new FormData(e.currentTarget);
+
+  console.log(data.get('name'));
+  console.log(data.get('description'));
+  console.log(data.get('color'));
+}
 
 const ProjectForm = (props: iProps) => {
+  const cancelHandler = () => {
+    props.setShowPopup(!props.showPopup)
+  }
   return (
     <div className="project-form">
       <Modal
         title="Create a New  Project"
         open={props.showPopup}
-        onCancel={() => props.setShowPopup(!props.showPopup)}
-        onOk={() => { props.setShowPopup(!props.showPopup) }}
+        footer={null}
+        onCancel={cancelHandler}
+        okButtonProps={{ form: 'category-editor-form', htmlType: 'submit' }}
       >
-        <form action="">
-          <Input label="Project Name" required />
-          <Input label="Description" />
-          <Input label="Color" type="color" required />
-        </form>
+        <Form onSubmitCapture={submitHandler}>
+          <Input name="name" label="Project Name" required />
+          <Input name="description" label="Description" />
+          <Input name="color" label="Color" type="color" required />
+          <div style={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+            <Button style={{ marginRight: 10 }} onClick={cancelHandler}>Cancel</Button>
+            <Button style={{ marginRight: 10 }} htmlType='submit'>Submit</Button>
+          </div>
+        </Form>
       </Modal>
     </div >
   )
