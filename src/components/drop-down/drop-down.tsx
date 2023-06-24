dd79f532e9fd8bf8114685303cbdae12b4cbb580
Mariam Hasanat <201011@ppu.edu.ps>
2023-06-24 22:23:18 +0300

    working without errors , but without correct render

--- src/components/drop-down/drop-down.tsx
+++ src/components/drop-down/drop-down.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CaretDownFilled } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Dropdown, Space } from 'antd';
 
+import { Dropdown, Space, Menu } from 'antd';
 
+interface IProName {
+  label: string,
+  key: string
+}
 const DropDown = (props: any) => {
-  const items : MenuProps['items'] = props.projects;
+  const items: MenuProps['items'] = props.projects;
+  const [dropdownLabel, setDropdownLabel] = useState<string>(' Projects');
   
+  const handleItemClick = (e: any) => {
+    setDropdownLabel(e.key);
+  };
+
+  const menuItems = items?.map((item: any) => (
+    <Menu.Item key={item.label}>{item.label}</Menu.Item>
+  ));
+
+  const menu = <Menu>{menuItems}</Menu>;
+
   return (
-    <Dropdown menu={{ items }} trigger={['click']} >
+    <Dropdown
+      trigger={['click']}
+      menu={{ onClick: handleItemClick, items: items }}
+    >
       <a href='./' onClick={(e) => e.preventDefault()}>
         <Space style={{ fontSize: '15px', color: '#52469C' }}>
-          Projects
+          {dropdownLabel}
           <CaretDownFilled />
         </Space>
       </a>
