e3e049da0d686d4de787412159982510d9cf7572
Mariam <201011@ppu.edu.ps>
2023-06-25 13:23:05 +0300

    save the state of task description even navigate to another page

--- src/components/new-task-form/new-task-form.tsx
+++ src/components/new-task-form/new-task-form.tsx
@@ -11,17 +11,20 @@ const NewTaskForm = (props: any) => {
   const [timeInSecond, setTimeInSecond] = React.useState<number>(0);//getTimeInSeconds(Number(localStorage.getItem("startTime")?.toString()) || 0)
   const [dropdownLabel, setDropdownLabel] = React.useState<string>(localStorage.getItem('projectName')?.toString() || 'Projects');
   const [isRunning, setIsRunning] = React.useState<boolean>((Number(localStorage.getItem("startTime")?.toString()) > 0));
-  const [taskDescription, setTaskDescription] = React.useState<string>('');
+  const [taskDescription, setTaskDescription] = React.useState<string>(localStorage.getItem("taskDescription")?.toString() || '');
 
   useEffect(() => {
     //Save the stopwatch time to localStorage whenever it changes
     localStorage.setItem('stopwatchTime', timeInSecond.toString());
   }, [timeInSecond]);
 
+  useEffect(() => {
+    localStorage.setItem("taskDescription", taskDescription)
+  }, [taskDescription])
 
   return (
     <form className='new-task-form'>
-      <Input disabled={isRunning} placeholder="Task Description" type='string' style={{ width: '300px', marginRight: '50px', height: '30px' }} value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)} />
+      <Input required={true} disabled={isRunning} placeholder="Task Description" type='string' style={{ width: '300px', marginRight: '50px', height: '30px' }} value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)} />
       <DropDown projects={props.projects} dropdownLabel={dropdownLabel} setDropdownLabel={setDropdownLabel} isRunning={isRunning} />
       <StopWatch timeInSecond={timeInSecond} />
       <ControlBtn setTimeInSecond={setTimeInSecond} setDropdownLabel={setDropdownLabel} isRunning={isRunning} setIsRunning={setIsRunning} setTaskDescription={setTaskDescription} />
