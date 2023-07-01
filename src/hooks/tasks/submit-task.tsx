01bd3244a0c7a0387a9df3b4b8d417f98da9a6ba
Mariam Hasanat <201011@ppu.edu.ps>
2023-07-01 20:39:06 +0200

    create the submit task hook

--- src/hooks/tasks/submit-task.tsx
+++ src/hooks/tasks/submit-task.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import showMessage from '../../utils/message/message';
+import submitTask from '../../services/tasks/submit-task';
 
-const submitTaskHandler = () => {
-  return (
-    <div>submitTaskHandler</div>
-  )
+export interface ITask {
+  projectId: string,
+  beginTime: string,
+  endTime?: string,
+  description: string,
+}
+const useTask = () => {
+
+  const submitTaskHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = new FormData(e.currentTarget);
+    const task: ITask = {
+      projectId: data.get('projectId') as string,
+      beginTime: data.get('beginTime') as string,
+      endTime: data.get('endTime') as string,
+      description: data.get('description') as string,
+    }
+    if (await submitTask(task)) {
+      showMessage('success', "task created successfully")
+    }
+  }
+  return {
+    submitTaskHandler
+  }
 }
 
-export default submitTaskHandler
+export default useTask
