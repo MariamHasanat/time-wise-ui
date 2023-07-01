e8655f80691efadc201c25f70970eea70b7c9aa3
Mariam Hasanat <201011@ppu.edu.ps>
2023-07-01 19:43:04 +0200

    create the submit-task service

---
+++ src/services/tasks/submit-task.ts
@@ -0,0 +1,40 @@
+import { ITask } from "../../types/task-interface";
+import showMessage from "../../utils/message/message";
+
+const submitTask = async (props: ITask) => {
+
+  const token: string = localStorage.getItem("token") || "";
+  if (!token.length) {
+    showMessage("error", "you are logged out, invalid token")
+  }
+  return fetch("http://localhost:3001/tasks", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'token': token
+    },
+    body: JSON.stringify({ ...props })
+  })
+    .then(async response => {
+      // console.log(response.status);
+      if (response.status === 401 || response.status === 403) {
+        showMessage('error', 'Please log in to continue');
+        response.json().then(() => localStorage.setItem('token', "invalid"));
+        return false;
+      } else if (response.status === 200) {
+        showMessage('success', `task begins successfully`);
+
+        return true;
+      } else {
+        showMessage('error', 'An unexpected error occurred');
+        throw new Error('Unexpected response status');
+      }
+    })
+    .catch(error => {
+      showMessage('error', error);
+      return false;
+    }
+    )
+}
+
+export default submitTask;
