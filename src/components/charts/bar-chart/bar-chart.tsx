a8c5f7d55898c70b55c6520e69bfce48549e6b04
Manolya Tamimi <tamimimanolya@gmail.com>
2023-07-01 16:10:35 +0300

    adding data sample

--- src/components/charts/bar-chart/bar-chart.tsx
+++ src/components/charts/bar-chart/bar-chart.tsx
@@ -1,55 +1,63 @@
 import { BarChart, Bar, YAxis, CartesianGrid, XAxis, Tooltip, Legend } from "recharts"
 
-const data = [
+const timeline = [
   {
-    "name": "Dec 30",
-    "time-wise": 4000,
-    "frying-nemo": 2400
+    "date": "Dec 3, 20220",
+    "time-wise": 0,
+    "frying-nemo": 1,
   },
   {
-    "name": "Dec 31",
-    "time-wise": 3000,
-    "frying-nemo": 1398
+    "date": "Dec 3, 20221",
+    "time-wise":2,
+    "frying-nemo": 1
   },
   {
-    "name": "Jan 1",
-    "time-wise": 2000,
-    "frying-nemo": 9800
+    "date": "Jan 1, 2022",
+    "time-wise": 0,
+    "frying-nemo": 0
   },
   {
-    "name": "Jan 2",
-    "time-wise": 2780,
-    "frying-nemo": 3908
+    "date": "Jan 2, 2022",
+    "time-wise": 1,
+    "frying-nemo": 2
   },
   {
-    "name": "Jan 3",
-    "time-wise": 2780,
-    "frying-nemo": 3908
+    "date": "Jan 3, 2022",
+    "time-wise": 3,
+    "frying-nemo": 1,
+    "graduation-project": 1
   },
   {
-    "name": "Jan 4",
-    "time-wise": 5780,
-    "frying-nemo": 3908
+    "date": "Jan 4, 2022",
+    "time-wise": 3,
+    "frying-nemo": 2
   },
   {
-    "name": "Jan 5",
-    "time-wise": 2780,
-    "frying-nemo": 3008
+    "date": "Jan 5, 2022",
+    "time-wise": 2,
+    "frying-nemo": 2,
+    "typing-test": 1,
   },
 ]
 
+const projects = [
+  { name: "time-wise", color: "#52469C" },
+  { name: "frying-nemo", color: "#ffa344" },
+  { name: "typing-test", color: "#f0db12" },
+  { name: "graduation-project", color: "red" },
+]
 const MyBarChart = () => {
   return (
     <div className="bar-chart">
-        <BarChart barSize={20} barGap={10} width={730} height={250} data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="time-wise" stackId="a" fill="#52469C" />
-          <Bar dataKey="frying-nemo" stackId="a" fill="#c4d3fa" />
-        </BarChart>
+      <BarChart barSize={20} barGap={10} width={730} height={250} data={timeline}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        {projects.map((project) =>
+          <Bar dataKey={project.name} stackId="a" fill={project.color} />)}
+      </BarChart>
     </div>
   )
 }
