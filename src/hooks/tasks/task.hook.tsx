ff55f65f10a964f7815f2c8ff1190d64b1fa4669
Mariam <201011@ppu.edu.ps>
2023-07-02 13:38:28 +0300

    create the useTask hook which get all tasks once enter the page , and create the task when needed

---
+++ src/hooks/tasks/task.hook.tsx
@@ -0,0 +1,50 @@
+// import React from 'react'
+import { useEffect, useState } from 'react';
+import TaskAPI, { comingTasks } from '../../services/tasks/submit-task';
+import showMessage from '../../utils/message/message';
+
+export interface ITask {
+  projectId: string,
+  beginTime: string,
+  endTime?: string,
+  description: string,
+}
+
+const api = new TaskAPI();
+
+const useTask = () => {
+
+  const [comingState, setComingState] = useState<Array<comingTasks>>([]);
+
+  useEffect(() => {
+    api.getTasks()
+      .then((tasks) => setComingState(tasks))
+      .catch(error => {
+        showMessage('error', error);
+      })
+  }, [])
+
+  const add = (task: ITask) => {
+
+    api.createTask(task)
+      .then(async success => {
+        let tasks = comingState;
+        if (success) {
+          showMessage('success', "task submitted successfully");
+          tasks = await api.getTasks();
+          setComingState(tasks)
+        } else {
+          showMessage('error', "failed submitted task")
+        }
+        setComingState(tasks);
+
+      })
+      .catch(error => {
+        showMessage('error', error);
+      })
+  }
+
+  return { comingState, add }
+}
+
+export default useTask
