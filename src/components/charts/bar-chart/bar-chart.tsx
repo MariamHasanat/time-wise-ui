cdfd0dbc00c998ebe3841b4bebfd6d4337426188
Manolya Tamimi <tamimimanolya@gmail.com>
2023-07-02 12:15:58 +0300

    resizing

--- src/components/charts/bar-chart/bar-chart.tsx
+++ src/components/charts/bar-chart/bar-chart.tsx
@@ -2,61 +2,79 @@ import { BarChart, Bar, YAxis, CartesianGrid, XAxis, Tooltip, Legend } from "rec
 
 const timeline = [
   {
-    "date": "Dec 3, 20220",
+    "date": "Jan 21",
     "time-wise": 0,
     "frying-nemo": 1,
   },
   {
-    "date": "Dec 3, 20221",
-    "time-wise":2,
+    "date": "Jan 22",
+    "time-wise": 2,
     "frying-nemo": 1
   },
   {
-    "date": "Jan 1, 2022",
+    "date": "Jan 23",
     "time-wise": 0,
     "frying-nemo": 0
   },
   {
-    "date": "Jan 2, 2022",
+    "date": "Jan 24",
     "time-wise": 1,
     "frying-nemo": 2
   },
   {
-    "date": "Jan 3, 2022",
+    "date": "Jan 25",
     "time-wise": 3,
     "frying-nemo": 1,
     "graduation-project": 1
   },
   {
-    "date": "Jan 4, 2022",
+    "date": "Jan 26",
     "time-wise": 3,
     "frying-nemo": 2
   },
   {
-    "date": "Jan 5, 2022",
+    "date": "Jan 27",
     "time-wise": 2,
     "frying-nemo": 2,
     "typing-test": 1,
   },
-]
-
-
-// Orange: #FF9800
-// Red: #F44336
-// Purple: #9C27B0
+  {
+    "date": "Jan 28",
+    "time-wise": 1,
+    "frying-nemo": 2
+  },
+  {
+    "date": "Jan 29",
+    "time-wise": 3,
+    "frying-nemo": 1,
+    "graduation-project": 1
+  },
+  {
+    "date": "Jan 30",
+    "time-wise": 3,
+    "frying-nemo": 2
+  },
+  {
+    "date": "Jan 31",
+    "time-wise": 2,
+    "frying-nemo": 2,
+    "typing-test": 1,
+  },
+];
 
 const projects = [
   { name: "typing-test", color: "#FFEB3B" },
   { name: "frying-nemo", color: "#FF9800" },
   { name: "graduation-project", color: "#F44336" },
   { name: "time-wise", color: "#9C27B0" },
-]
+];
+
 const MyBarChart = () => {
   return (
     <div className="bar-chart">
-      <BarChart barSize={20} barGap={10} width={730} height={250} data={timeline}>
+      <BarChart barSize={20} barGap={10} width={1000} height={250} data={timeline}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
+        <XAxis dataKey={"date"} />
         <YAxis />
         <Tooltip />
         <Legend />
