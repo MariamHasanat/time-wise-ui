8123bba53e98da09c0245bb0e61dd38498044914
Mariam Hasanat <201011@ppu.edu.ps>
2023-07-04 18:16:14 +0300

    create complete task service

--- src/services/tasks/submit-task.ts
+++ src/services/tasks/submit-task.ts
@@ -12,6 +12,11 @@ export interface comingTasks {
   'totalTimeInSeconds': string
 }
 
+export interface ITaskInfo {
+  '_id': string,
+  'endTime': string,
+}
+
 class TaskAPI {
   private API: string = `http://localhost:3001`;
   private token: string = localStorage.getItem("token") || "";
@@ -40,6 +45,22 @@ class TaskAPI {
     };
     return fetch(`${this.API}/tasks`, optional)
       .then(res => res.status === 201)
+      .catch(error => showMessage('error', error))
+
+  };
+  completeTask = (taskInfo: ITaskInfo) => {
+    console.log(taskInfo);
+
+    const optional: RequestInit = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'token': this.token
+      },
+      body: JSON.stringify(taskInfo.endTime)
+    };
+    return fetch(`${this.API}/tasks/${taskInfo._id}`, optional)
+      .then(res => res.status === 200)
       .catch(error => showMessage('error', error))
 
   };
