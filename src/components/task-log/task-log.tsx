f86fe0469fb41a71257cbe97bc367f0f01576eb7
Mariam Hasanat <201011@ppu.edu.ps>
2023-07-04 09:48:52 +0300

    get all tasks and pass it through the task log component

--- src/components/task-log/task-log.tsx
+++ src/components/task-log/task-log.tsx
@@ -4,35 +4,41 @@ import { ClockCircleOutlined, EditOutlined } from "@ant-design/icons"
 import { useState } from "react";
 import EditTaskPopup from "../edit-task/edit-task";
 import DeleteConfirmation from "../delete-confirmation/delete-confirmation";
+import { comingTasks } from "../../services/tasks/submit-task";
 
-const TaskLog = () => {
+
+const TaskLog = (props: any) => {
+  const allTasks = props.allTasks;
+  console.log("from task log", allTasks);
+
   const [editMode, setEditMode] = useState<boolean>(false);
   return (
-    <form className="task-log">
+
+    allTasks.map((task: comingTasks, key:number) => (<form className="task-log" key={key}>
       <span className="color">&nbsp;</span>
       <div className="task-desc">
-        <label style={{ fontSize: "15px", color: "#52469C", width: 300 }}>Description</label>
-        <label style={{ fontSize: "10px", color: "#7489C1" }} >projectName</label>
+        <label style={{ fontSize: "15px", color: "#52469C", width: 300 }}>{task.description}</label>
+        <label style={{ fontSize: "10px", color: "#7489C1" }} >{task.projectName}</label>
       </div>
-      <label >15 mins</label>
+      <label >{task.totalTimeInSeconds} mins</label>
       <div className="task-time">
         <ClockCircleOutlined style={{ "fontSize": "15px", "margin": "10px" }} />
         <div className="task-date-time">
-          <label> &nbsp;3:30 - 3:45pm</label>
-          <label className="task-dates" >June 19 - June 20</label>
+          <label> &nbsp;{task.beginTime} - {task.endTime}</label>
+          <label className="task-dates" >{task.endTime}</label>
         </div>
       </div>
       <div className="control-btns">
         <Button
           onClick={() => setEditMode(true)}
-          style={{ "backgroundColor": "transparent", "border": "none", "boxShadow": "none", "padding": 0, "marginRight":"10px"}}
+          style={{ "backgroundColor": "transparent", "border": "none", "boxShadow": "none", "padding": 0, "marginRight": "10px" }}
         >
           <EditOutlined />
         </Button>
         <DeleteConfirmation />
       </div>
       <EditTaskPopup editMode={editMode} setEditMode={setEditMode} description="none" />
-    </form>
+    </form>))
   )
 }
 
