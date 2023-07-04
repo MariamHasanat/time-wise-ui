53bcb97655cf28ee77a1c5c85d982a9a3aea38c5
Manolya Tamimi <tamimimanolya@gmail.com>
2023-07-04 22:17:29 +0300

    fetching user data

--- src/components/user-card/user-card.tsx
+++ src/components/user-card/user-card.tsx
@@ -2,17 +2,20 @@ import './user-card.css';
 import { Button } from 'antd';
 import { ExportOutlined } from "@ant-design/icons"
 import { useNavigate } from 'react-router';
-
-const UserCard = () => {
+interface userData {
+  username: string,
+  email: string
+}
+const UserCard = (user: userData) => {
   const navigate = useNavigate();
   return (
     <div className='user-card'>
       <div className='profile-pic'>
-        <img src='/cat.png' alt='avatara'/>
+        <img src='/cat.png' alt='avatara' />
       </div>
       <div className='user-details'>
-        <h4 className='username' style={{ display: 'flex', justifyContent: 'space-between' }}>username</h4>
-        <h5 className='useremail'>username@company.com</h5>
+        <h4 className='username' style={{ display: 'flex', justifyContent: 'space-between' }}>{user.username || "user"}</h4>
+        <h5 className='useremail'>{user.email || "username@company.com"}</h5>
       </div>
       <Button type='primary' style={{ border: 0 }} className='logout-btn'
         onClick={() => {
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -3,15 +3,49 @@ import { default as BarChart } from "../../components/charts/bar-chart";
 import { default as PieChart } from "../../components/charts/pie-chart";
 import { DatePicker } from 'antd';
 import UserCard from '../../components/user-card/user-card';
+import { useEffect, useState } from 'react';
+import showMessage from '../../utils/message/message';
 
 const { RangePicker } = DatePicker;
+interface userData {
+  username: string,
+  email: string
+}
 const Dashboard = () => {
+  const [user, setUser] = useState<userData>({ username: "", email: "" });
+  const token: string = localStorage.getItem('token') || "";
+
+  const fetchUserData = async () => {
+    fetch('http://localhost:3001/user', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "token": token
+      }
+    })
+      .then(res => {
+        if (res.status === 200) {
+          res.json().then((res) => {
+            setUser(res)
+            return true;
+          }
+          )
+        }
+      }).catch(err => {
+        showMessage("error", err);
+        return undefined;
+      })
+  }
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
   return (
     <div className="dashboard">
       <div className='top'>
-          <UserCard />
+        <UserCard username={user.username} email={user.email} />
         <div className="range-picker">
-          <RangePicker placement='bottomRight'/>
+          <RangePicker placement='bottomRight' />
           <PieChart />
         </div>
       </div>
