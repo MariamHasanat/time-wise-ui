336697b55c61fdb5343c8000e1c9b4e13dc85279
Mariam <201011@ppu.edu.ps>
2023-07-06 10:47:46 +0300

    delete the habal

--- src/components/task-log/task-log.tsx
+++ src/components/task-log/task-log.tsx
@@ -66,38 +66,3 @@ const TaskLog = (props: any) => {
 }
 
 export default TaskLog;
-// {/* 
-//<>{
-//       allTasks.length ?
-//         allTasks.map((task: comingTasks, key: number) => (
-//           task.status === "stopped"
-//             ?
-//             <form className="task-log" key={key}>
-//               <span className="color" style={{ backgroundColor: task.projectColor }}>&nbsp;</span>
-//               <div className="task-desc">
-//                 <label style={{ fontSize: "15px", color: "#52469C", width: 300 }}>{task.description}</label>
-//                 <label style={{ fontSize: "10px", color: "#7489C1" }} >{task.projectName}</label>
-//               </div>
-//               <label >{whatTheTime(task.totalTimeInSeconds)}</label>
-//               <div className="task-time">
-//                 <ClockCircleTwoTone style={{ "fontSize": "22px", "margin": "5px", "color": "#f9ff00" }} /> &nbsp;&nbsp;
-//                 <div className="task-date-time">
-//                   <label> &nbsp;{timeInHoursAndMinutes(task.beginTime)} - {timeInHoursAndMinutes(task.endTime)}</label>
-//                   <label className="task-dates" >{timeAsADate(task.beginTime)} - {timeAsADate(task.endTime)}</label>
-//                 </div>
-//               </div>
-//               <div className="control-btns">
-//                 <Button
-//                   onClick={() => setEditMode(true)}
-//                   style={{ "backgroundColor": "transparent", "border": "none", "boxShadow": "none", "padding": 0, "marginRight": "10px" }}
-//                 >
-//                   <EditTwoTone style={{ "fontSize": "22px", "margin": "5px" }} />
-//                 </Button>
-//                 <DeleteConfirmation />
-//               </div>
-//               <EditTaskPopup editMode={editMode} setEditMode={setEditMode} description="none" />
-//             </form>
-//             :
-//             <div key={key}></div>))
-//         : <NoTaksFound />
-//     }</> */}
